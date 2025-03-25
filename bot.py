8d264d3144224b769f9804eef608fd3d860a70d4
botdepoy <hha804336@gmail.com>
2025-03-25 13:12:16 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -345,7 +345,7 @@ RESPONSE_DATA = {
 
                                                 ,
                                     "buttons": [
-                                                [InlineKeyboardButton(text="办理流程",url="https://botdepoy.github.io/NewTelegrambot/form.html?type=rental")
+                                                [InlineKeyboardButton(text="办理流程",url="https://t.me/HWLDSWFW_bot/Myapp?type=rental")
                                                 ,InlineKeyboardButton(text="💬 联系客服",url="https://t.me/LUODISWKF?start=Hi%20I%20need%20help")],
                                                 [InlineKeyboardButton("🔙 返回", callback_data="🏨 酒店与租凭")]
                                     ]            
@@ -370,7 +370,7 @@ RESPONSE_DATA = {
 
                                                 ,
                                     "buttons": [
-                                                [InlineKeyboardButton(text="办理流程",url="https://botdepoy.github.io/NewTelegrambot/form.html?type=visa"),
+                                                [InlineKeyboardButton(text="办理流程",url="https://t.me/HWLDSWFW_bot/Myapp?type=visa"),
                                                 InlineKeyboardButton(text="💬 联系客服",url="https://t.me/LUODISWKF?start=Hi%20I%20need%20help")],                    
                                                 [InlineKeyboardButton("🔙 返回", callback_data="✈️ 交通服务")]
                                     ]            
